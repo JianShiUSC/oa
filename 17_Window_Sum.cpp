592c02936a2a82f4be2aa806efbc9995e6d4a074
Jian Shi <jians@usc.edu>
2016-11-28 00:52:30 -0800

    amazon oa

--- 17_Window_Sum.cpp
+++ 17_Window_Sum.cpp
@@ -15,3 +15,27 @@ int main() {
     vector<int> A = {1,2,3,4,5};
     getSum(A, 2);
 }
+
+
+vector<int> getSum(vector<int> A, int k) {
+    vector<int> res;
+    if(A.empty()) return res;
+    int sum = 0;
+    int size = A.size();
+
+    for(int i = 0; i < size; i++) {
+        if(i < k - 1) {
+            sum += A[i];
+        } else {
+            sum += A[i];
+            res.push_back(sum);
+            sum -= A[i-k+1];
+        }
+    }
+    return res;
+}
+
+int main() {
+    vector<int> A = {1,2,3,4,5};
+    getSum(A, 2);
+}
